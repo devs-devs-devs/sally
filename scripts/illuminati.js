cd606e334af69b079611621bca3aa513ef49e1ed
ChrisDBrown <owner@chrisdbrown.co.uk>
2017-06-16 21:07:21 +0100

    feat(replies): Add test emoji reaction script

---
+++ scripts/illuminati.js
@@ -0,0 +1,20 @@
+// Description:
+//   General DDD guff
+// 
+// Dependencies:
+//   None
+// 
+// Configuration:
+//   None
+// 
+// Commands:
+//   hubot - blows a kiss
+// 
+// Author:
+//   ChrisDBrown <owner@chrisdbrown.co.uk>
+
+module.exports = function(robot) {
+  robot.hear(new RegExp(robot.name, "i"), function(res) {
+    robot.adapter.client.web.reactions.add('kissing_heart', {channel: res.message.room, timestamp: res.message.id});
+  });
+}
