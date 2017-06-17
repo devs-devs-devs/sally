9112d1ebc79e09a594ccd620d02ca33d1266cb84
ChrisDBrown <owner@chrisdbrown.co.uk>
2017-06-17 09:28:23 +0100

    chore(lint): Add eslint to circle tests

--- scripts/illuminati.js
+++ scripts/illuminati.js
@@ -1,22 +1,25 @@
 // Description:
 //   General DDD guff
-// 
+//
 // Dependencies:
 //   None
-// 
+//
 // Configuration:
 //   None
-// 
+//
 // Commands:
 //   hubot - blows a kiss
-// 
+//
 // Author:
 //   ChrisDBrown <owner@chrisdbrown.co.uk>
 
-module.exports = function(robot) {
-  robot.hear(new RegExp(robot.name, "i"), function(res) {
-    if (robot.adapterName == 'slack') {
-      robot.adapter.client.web.reactions.add('kissing_heart', {channel: res.message.room, timestamp: res.message.id});
+module.exports = (robot) => {
+  robot.hear(new RegExp(robot.name, 'i'), (res) => {
+    if (robot.adapterName === 'slack') {
+      robot.adapter.client.web.reactions.add(
+        'kissing_heart',
+        { channel: res.message.room, timestamp: res.message.id },
+      );
     }
   });
-}
+};
